e50ededb2e69bb720db3e4fa025f10fb81cac15a
Arkadiusz Halicki <a.halicki@partner.samsung.com>
2017-02-15 18:42:32 +0100

    init

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+var notify = require('gulp-notify');
+var bower = require('gulp-bower');
+
+var config = {
+  bowerDir: './bower_components'
+};
+
+gulp.task('default', function() { });
